8bcdd2bbe29c5f56b1d2e5dc6b6bd85169a67c42
Arpad Borsos <arpad.borsos@googlemail.com>
2014-03-04 12:42:00 +0100

    work with a static array, since we are making changes to it that may lead to missed files

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ module.exports = function (builder) {
 	builder.hook('before scripts', function (pkg, fn) {
 		if (!pkg.root)
 			return fn();
-		pkg.config.scripts.forEach(function (file) {
+		pkg.config.scripts.slice().forEach(function (file) {
 			var fullPath = pkg.path(file);
 			try {
 				var contents = fs.readFileSync(fullPath, 'utf8');
